d3017e62df05bdf5f876f2be4ea29f0fbab651d9
THEGUYii <36053849+THEGUYii@users.noreply.github.com>
2018-02-15 15:21:17 -0600

    3
    day 3

---
+++ Template/sketch.js
@@ -0,0 +1,14 @@
+// Template (Project title)
+// Noah Flynn
+// Date
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {}
+
+function keyIsPressed() {
+
+}
--- first assignment/sketch.js
+++ first assignment/sketch.js
@@ -1,16 +1,29 @@
 // mouse and keyboard interaction
 // Noah Flynn
-// Feb 9, 2018
-
+// Feb 14, 2018
+let reset;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  noStroke();
   background(192, 192, 192);
+  reset = true;
 }
 
-function draw() {}
+function draw() {
+  fill(random(255), random(255), random(255));
+  ellipse(random(windowWidth), random(windowHeight), 50, 50);
+}
 
-function randomBalls() {
-  ellipse(random(windowWidth), random(windowHeight), 50, 255);
+function keyTyped() {
+  if (key === " ") {
+    reset = !reset;
+    return false;
+  }
+}
+
+
+function mouseClicked() {
+  if (reset === true) {
+    background(192, 192, 192);
+  }
 }
