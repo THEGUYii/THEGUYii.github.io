2bd471dc1c913244de9f776d3fe4c7873c637fc7
THEGUYii <36053849+THEGUYii@users.noreply.github.com>
2018-03-24 13:56:06 -0600

    u
    u

--- terrain/sketch.js
+++ terrain/sketch.js
@@ -1,10 +1,14 @@
 // Terrain Generation
-// Dan Schellenberg - replace with your name
-// Mar 19, 2018
+// Naoh Flynn
+// Mar 23, 2018
 
 let heights = [];
 let numberOfRectangles;
-let rectWidth = width / numberOfRectangles;
+let flag;
+
+function preload() {
+  flag = loadImage("images/canada-flag-icon-32.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,44 +19,48 @@ function setup() {
 function draw() {
   background(255);
   displayTerrain();
+  plantFlag();
+}
+
+function plantFlag() {
+  let highestX;
+  let rectWidth = width / numberOfRectangles;
+  let tallest = 0;
+  for (let i=0; i<heights.length; i++) {
+    if (heights[i] > tallest) {
+      tallest = heights[i];
+      highestX = i*rectWidth;
+    }
+  }
+
+  // height line
+  let highestY = height - tallest;
+  // stroke(255, 0, 0);
+  // line(0, highestY, width, highestY);
+
+  // flag
+  // fill(255, 0, 0);
+  // ellipse(highestX, highestY, 50, 50);
+  image(flag, highestX, highestY-flag.height);
 }
 
 function generateInitialTerrain(numberOfRectangles) {
   let time = 0;
-  let dt = 0.001;
+  let dt = 0.002;
 
-  for (let i = 0; i < numberOfRectangles; i++) {
-    let currentHeight = noise(time) * 700;
-    time += dt;
+  for (let i=0; i<numberOfRectangles; i++) {
+    let currentHeight = noise(time)*500;
     heights.push(currentHeight);
+    time += dt;
   }
-}
-
-function placeFlag() {
-  let tallest = 0;
-  let highestX = 0;
-  for (let i = 0; i < heights.length; i++) {
-    if (heights[i] > tallest) {
-      tallest = heights[i];
-      highestX = i * rectWidth;
-    }
-  }
-
-  let highestY = height - tallest;
-  stroke(255, 0, 0);
-  line(0, tallest, width, tallest);
-
-  // Flag
-  fill(255, 0, 0);
-  ellipse(highestX, highestY, 20, 20);
 }
 
 function displayTerrain() {
   let rectWidth = width / numberOfRectangles;
   rectMode(CORNERS);
-  fill(255);
+  fill(0);
   stroke(0);
-  for (let i = 0; i < numberOfRectangles; i++) {
-    rect(i * rectWidth, height, (i + 1) * rectWidth, height - heights[i]);
+  for (let i=0; i<numberOfRectangles; i++) {
+    rect(i*rectWidth, height, (i+1)*rectWidth, height - heights[i]);
   }
 }
